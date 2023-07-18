552a3d48b29fab32405057fe308e1e0af4727858
David Rolón <rolondavid@outlook.com>
2023-07-18 17:17:28 -0300

    Correccion de index, funcionalidad de curriculum, pagina sobre mi, se agregaron logos de tecnologias

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('sobre_mi', views.sobre_mi, name="sobre_mi"),
+    path('tecnologias', views.tecnologias, name="tecnologias"),
 ]
--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse, Http404, JsonResponse
 
 def index(request, template_name='my_portfolio/index.html'):
     return render(request, template_name)
+
+def sobre_mi(request, template_name='my_portfolio/sobre_mi.html'):
+    return render(request, template_name)
+
+def tecnologias(request, template_name='my_portfolio/tecnologias.html'):
+    return render(request, template_name)
