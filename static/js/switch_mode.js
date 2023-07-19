f8e9d3d12a7e168cdebbcbf3769078ae59baad36
David Rolón <rolondavid@outlook.com>
2023-07-19 12:50:13 -0300

    fix: memoria de tema
    Se utilizaron cookies para recordar el ultimo tema utilizado

--- static/js/switch_mode.js
+++ static/js/switch_mode.js
@@ -1,15 +1,40 @@
+function setThemeCookie(name) {
+    document.cookie = `theme=${name}`;
+}
+
 const lightTheme = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "light");
     document.querySelector("#icon-theme").setAttribute("class", "bi bi-sun-fill");
     document.querySelector("#icon-logo").setAttribute("class", "icon-logo-dark");
+    setThemeCookie("light");
 }
 
 const darkTheme = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "dark");
     document.querySelector("#icon-theme").setAttribute("class", "bi bi-moon-fill");
     document.querySelector("#icon-logo").setAttribute("class", "icon-logo-light");
+    setThemeCookie("dark");
+}
+
+function getCookies() {
+    return document.cookie
+        .split(';')
+        .map(cookie => cookie.split('='))
+        .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+}
+
+function getThemeCookie() {
+    return getCookies().theme;
 }
 
 const switchTheme = () => {
-    document.querySelector("body").getAttribute("data-bs-theme") === "light" ? darkTheme() : lightTheme();
+    getThemeCookie() === "light" ? darkTheme() : lightTheme();
+}
+
+function verifyThemeCookie() {
+    theme = getThemeCookie();
+    if (theme === "light") { lightTheme(); }
+    else if (theme === "dark") { darkTheme(); }
+    else { darkTheme(); }
+    console.log("papa")
 }
