51af5050bbdc7db5c4207ea971d04b7ee3b68c2b
David Rolón <rolondavid@outlook.com>
2023-07-19 23:14:10 -0300

    style: Carga de imagenes faltantes

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -18,10 +18,11 @@ def tecnology(request, template_name='my_portfolio/tecnology.html'):
     return render(request, template_name, context=context)
 
 def education(request, template_name='my_portfolio/education.html'):
-    education = [("Data Scientist", "Abril 2022", "Cursando", "EducacionIT", "img.webp", ["Base de datos", "ETL", "Probabilidad", "Estadistica", "Analisis de datos", "Visualizacion de Datos", "Machine Learning", "Deep Learning", "Optimización de modelos"]),
-    ("JAVA Backend Developer", "Diciembre 2022", "Junio 2023", "Programa ONE - Alura Oracle", "img.webp", ["Java", "MySQL", "GIT", "GitHub", "JDBC", "JPA", "API Rest", "Swagger"]),
-    ("Full Stack Web Developer", "Mayo 2022", "Mayo 2023", "EGG Cooperation", "img.webp", ["Java", "MySQL", "GIT", "GitHub", "JDBC", "JPA", "MVC", "ReactJs"]),
-    ("Técnico Superior en Automatización y Robótica", "2015", "2017", "UTN - Instituto Nacional de Profesorado Técnico", "cv.png"),
-    ("Técnico Electromécanico", "2008", "2014", "Instituto de Educación Técnica Manuel Belgrano", "cv.png")]
+    education = [("Data Scientist", "Abril 2022", "Cursando", "EducacionIT", "loading.png", ["Base de datos", "ETL", "Probabilidad", "Estadistica", "Analisis de datos", "Visualizacion de Datos", "Machine Learning", "Deep Learning", "Optimización de modelos"]),
+    ("JAVA Backend Developer", "Diciembre 2022", "Junio 2023", "Programa ONE - Alura Oracle", "programa_one.png", ["Java", "MySQL", "GIT", "GitHub", "JDBC", "JPA", "API Rest", "Swagger"]),
+    ("Full Stack Web Developer", "Mayo 2022", "Mayo 2023", "EGG Cooperation", "fullstack_web_developer.png", ["Java", "MySQL", "GIT", "GitHub", "JDBC", "JPA", "MVC", "ReactJs"]),
+    ("Reparación de Dispositivos Inteligentes", "Mayo 2019", "Septiembre 2019", "Centro de Formación Profesional 402", "dispositivos_Inteligentes.jpg"),
+    ("Técnico Superior en Automatización y Robótica", "2015", "2017", "UTN - Instituto Nacional de Profesorado Técnico", "tecnicatura_superior.jpeg"),
+    ("Técnico Electromécanico", "2008", "2014", "Instituto de Educación Técnica Manuel Belgrano", "tecnico_electromecanico.jpg")]
     context = {"education": education}
     return render(request, template_name, context=context)
