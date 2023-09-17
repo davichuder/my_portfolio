3c6ac3dd94271d2e8ba43131f5f5422f9d3c2d36
David Rolón <rolondavid@outlook.com>
2023-09-17 01:45:15 -0300

    fix: Se actualizaron modelos
    Se modificaron modelos, se realizaron las migraciones, y se agrego el modelo Pages a la pestaña de Admin

--- my_portfolio/admin.py
+++ my_portfolio/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from . import models
 
+admin.site.register(models.Pages)
 admin.site.register(models.Tecnology)
 admin.site.register(models.Experience)
 admin.site.register(models.Project)
 admin.site.register(models.Education)
+admin.site.register(models.Curriculum)
--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
+class Pages(models.Model):
+    name = models.CharField(max_length=64, blank=True, default="", unique=True)
+    url = models.CharField(max_length=64, blank=True, unique=True)
+    previous_url = models.CharField(max_length=64, blank=True)
+    next_url = models.CharField(max_length=64, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Pages"
+
+    def __str__(self):
+        return self.url
 
 class Tecnology(models.Model):
     name = models.CharField(max_length=32, unique=True)
@@ -8,6 +19,29 @@ class Tecnology(models.Model):
 
     class Meta:
         verbose_name_plural = "Tecnologies"
+
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    img_name = models.CharField(max_length=32)
+    description = models.TextField(max_length=512)
+    url_github = models.CharField(max_length=512, null=True, blank=True)
+    url_demo = models.CharField(max_length=512, null=True, blank=True)
+    tags = TaggableManager()
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Tags"
 
     def __str__(self):
         return self.name
@@ -25,23 +59,6 @@ class Experience(models.Model):
     def __str__(self):
         return self.job
 
-class Project(models.Model):
-    name = models.CharField(max_length=32, unique=True)
-    img_name = models.CharField(max_length=32)
-    description = models.TextField(max_length=512)
-    url_github = models.CharField(max_length=512, null=True, blank=True)
-    url_demo = models.CharField(max_length=512, null=True, blank=True)
-    tags = TaggableManager()
-
-    def __str__(self):
-        return self.name
-
-class Tag(models.Model):
-    name = models.CharField(max_length=32, unique=True)
-
-    def __str__(self):
-        return self.name
-
 class Education(models.Model):
     name = models.CharField(max_length=64)
     date_start = models.CharField(max_length=32)
@@ -52,6 +69,16 @@ class Education(models.Model):
 
     class Meta:
         verbose_name_plural = "Education"
+
+    def __str__(self):
+        return self.name
+    
+class Curriculum(models.Model):
+    name = models.CharField(max_length=64)
+    url = models.CharField(max_length=512)
+
+    class Meta:
+        verbose_name_plural = "Curriculums"
 
     def __str__(self):
         return self.name
